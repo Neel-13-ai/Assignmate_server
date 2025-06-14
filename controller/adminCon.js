bc1e4401e8976123c859a16a3bde934efeab6c36
Neel Patel <149805066+Neel-13-ai@users.noreply.github.com>
2025-06-14 18:51:15 +0530

    Update adminCon.js

--- controller/adminCon.js
+++ controller/adminCon.js
@@ -2,7 +2,6 @@ const{ PrismaClient } = require("@prisma/client");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
-const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const addAdmin = async (req, res) => {
